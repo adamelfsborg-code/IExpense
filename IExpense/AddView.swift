c36f68272052a80f9f993e53415967fd710f71d2
Adam Elfsborg <adamelfsborg.dev@gmail.com>
2024-07-18 19:08:25 +0200

    feat: core app

---
+++ IExpense/AddView.swift
@@ -0,0 +1,47 @@
+//
+//  AddView.swift
+//  IExpense
+//
+//  Created by Adam Elfsborg on 2024-07-18.
+//
+
+import SwiftUI
+
+struct AddView: View {
+    @Environment(\.dismiss) var dismiss
+    @State private var name = ""
+    @State private var type = "Personal"
+    @State private var amount = 0.0
+    
+    var expenses: Expenses
+    
+    let types = ["Personal", "Business"]
+    var body: some View {
+        NavigationStack {
+            Form {
+                TextField("Name", text: $name)
+                
+                Picker("Type", selection: $type) {
+                    ForEach(types, id: \.self) {
+                        Text("\($0)")
+                    }
+                }
+                
+                TextField("Amount", value: $amount, format: .currency(code: "USD"))
+                    .keyboardType(.decimalPad)
+            }
+            .navigationTitle("Add Expense")
+            .toolbar {
+                Button("Save") {
+                    let item = ExpenseItem(name: name, type: type, amount: amount)
+                    expenses.items.append(item)
+                    dismiss()
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    AddView(expenses: Expenses())
+}
